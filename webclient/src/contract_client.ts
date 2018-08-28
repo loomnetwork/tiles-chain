aceb50af978f64e4de1774a4030d293909fd6c7c
Matthew Campbell <hyper@hyperworks.nu>
2018-08-28 16:42:47 +0700

    consolidate ports

--- webclient/src/contract_client.ts
+++ webclient/src/contract_client.ts
@@ -17,8 +17,8 @@ export default class ContractClient {
 
     const client = new Client(
       'default',
-      'ws://127.0.0.1:46657/websocket',
-      'ws://127.0.0.1:9999/queryws'
+      'ws://127.0.0.1:46658/websocket',
+      'ws://127.0.0.1:46658/queryws'
     )
     // required middleware
     client.txMiddleware = [
